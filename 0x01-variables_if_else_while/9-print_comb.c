7f3452fde6f360e73cdfc5a43fcf2b01c510be16
ucheogbodo-25 <ucheogbodo25@gmail.com>
2023-03-19 13:28:51 -0700

    9-prints_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,7 +3,7 @@
 /**
  * main - main function
  *
- * Return: always 0 
+ * Return: always 0
  */
 
 int main(void)
@@ -13,8 +13,12 @@ int main(void)
 	for (i = 0 ; i < 10 ; i++)
 	{
 		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
+	putchar('\n');
 	return (0);
 }
