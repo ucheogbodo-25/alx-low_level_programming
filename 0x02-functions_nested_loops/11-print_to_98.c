16523327e318515208f3444143ae6b35746832ee
ucheogbodo-25 <ucheogbodo25@gmail.com>
2023-03-21 17:40:44 -0700

    11-print_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,9 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(",");
 		}
 		printf("\n");
 	}
@@ -24,7 +26,9 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+				if (i != 98)
+					printf(",");
 		}
 		printf("\n");
 	}
@@ -32,9 +36,10 @@ void print_to_98(int n)
 	{
 		for (i = n ; i >= 98 ; i--)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
 		}
 		printf("\n");
-	}	else if (n == 98)
-		printf("%d, ", n);
+	}	
+	else if (n == 98)
+		printf("%d", n);
 }
