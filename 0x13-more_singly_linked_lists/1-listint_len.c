822ebd28a8d0555e2a2e479b590e1dab5ffcf0ec
ucheogbodo25 <ucheogbodo25@gmail.com>
2023-04-29 13:14:20 -0700

    task 2

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,14 +8,14 @@
  */
 size_t listint_len(const listint_t *h)
 {
-    size_t count = 0;
-    const listint_t *current = h;
+	size_t count = 0;
+	const listint_t *current = h;
 
-    while (current != NULL)
-    {
-        count++;
-        current = current->next;
-    }
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
 
-    return count;
+	return (count);
 }
